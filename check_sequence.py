bbe3dad4f06fd797c434fc1ffa668f89148046ae
Simon Melouah <simon.melouah@mail.dcu.ie>
2016-11-25 15:26:41 +0000

    First commit

---
+++ check_sequence.py
@@ -0,0 +1,32 @@
+condition = True
+array = []
+
+def get_number(array=True):
+	while condition:
+		if array:
+			value = raw_input("Enter an array size: ")
+		else:
+			value = raw_input("Enter a number: ")
+		try:
+			value = int(value)
+			return value
+		except:
+			print "Woops! Not a number..."
+
+def check_if_list_contains_sequence(array):
+	sequence = "1, 2, 3"
+	sorted_array = ', '.join(str(i) for i in sorted(array))
+	print sorted_array
+	if sequence in sorted_array:
+		return True
+	else:
+		return False
+
+array_size = get_number()
+for i in range(0, array_size):
+	value = get_number(False)
+	array.append(value)
+
+print array
+check_result = check_if_list_contains_sequence(array)
+print check_result
